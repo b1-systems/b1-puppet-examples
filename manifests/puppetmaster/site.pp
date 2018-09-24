d23f17a9339e8d13ed9a8520b5f53f5823df7dc1
Peter Hoffmann <hoffmann@b1-systems.de>
2018-09-24 23:41:07 +0200

    Worked again on manifests and exercises.

--- manifests/puppetmaster/site.pp
+++ manifests/puppetmaster/site.pp
@@ -1,6 +1,6 @@
 # copyright: B1 Systems GmbH <info@b1-systems.de>, 2018
 # license:   GPLv3+, http://www.gnu.org/licenses/gpl-3.0.html
 
-# /opt/puppetlabs/puppet/bin/gem install r10k
-# ln -sv /opt/puppetlabs/puppet/bin/r10k \
-  /usr/local/bin
+node default {
+  notify { 'node default from Puppet Server': }
+}
