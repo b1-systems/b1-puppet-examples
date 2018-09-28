13764716371d092955ec4d72452ee728419865f7
Peter Hoffmann <hoffmann@b1-systems.de>
2018-09-28 13:46:58 +0200

    Worked on manifests puppet_enc.

--- manifests/puppet_enc/simpleenc.sh
+++ manifests/puppet_enc/simpleenc.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
+
 # copyright: B1 Systems GmbH <info@b1-systems.de>, 2018
 # license:   GPLv3+, http://www.gnu.org/licenses/gpl-3.0.html
 
-#!/bin/bash
 NODE="$1"
 ENCDATA='/etc/puppetlabs/code/encdata'
 #ENCDATA='/etc/puppet/encdata'
