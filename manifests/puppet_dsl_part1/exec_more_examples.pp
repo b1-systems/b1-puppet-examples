6c278fc847e3ecb5342064de8384f0c30b98741c
Peter Hoffmann <hoffmann@b1-systems.de>
2019-01-24 20:10:27 +0100

    Fixed typos.

--- manifests/puppet_dsl_part1/exec_more_examples.pp
+++ manifests/puppet_dsl_part1/exec_more_examples.pp
@@ -2,12 +2,12 @@
 # license:   GPLv3+, http://www.gnu.org/licenses/gpl-3.0.html
 
 exec { 'download ftp archive':
-  command => '/usr/bin/curl http://server/file.tgz > /srv/ftp/deploy.tgz':
+  command => '/usr/bin/curl http://server/file.tgz > /srv/ftp/deploy.tgz',
   onlyif  => '/usr/bin/rpm -q vsftp',
 }
 
 exec { 'download SLES ISO':
-  command     => 'wget -O/home/tux/sles11.iso http://suse.com/sles11.iso':
+  command     => 'wget -O/home/tux/sles11.iso http://suse.com/sles11.iso',
   environment => [ 'http_proxy=http://squid.example.com:3128' ],
   path        => [ '/usr/bin', '/bin' ],
   user        => 'tux',
