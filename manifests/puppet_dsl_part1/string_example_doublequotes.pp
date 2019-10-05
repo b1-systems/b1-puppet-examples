215ca489a9b6e447d05e24bfc7d56303ce4adfbd
Peter Hoffmann <hoffmann@b1-systems.de>
2019-10-05 16:05:01 +0200

    Use actually double quotes in this example.

--- manifests/puppet_dsl_part1/string_example_doublequotes.pp
+++ manifests/puppet_dsl_part1/string_example_doublequotes.pp
@@ -1,11 +1,7 @@
 # copyright: B1 Systems GmbH <info@b1-systems.de>, 2018
 # license:   GPLv3+, http://www.gnu.org/licenses/gpl-3.0.html
 
-$variable = 'Willkommen in der Beispiel GmbH
-
-Aktuelle Neuigkeiten:
-  - keine
-'
+$variable = "Willkommen in der Beispiel GmbH\n\nAktuelle Neuigkeiten:\n  - keine\n"
 
 file { '/etc/motd':
   ensure  => file,
