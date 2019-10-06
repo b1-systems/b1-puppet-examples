6da97852941c2a1734c3fe55d77b20568e1577bb
Peter Hoffmann <hoffmann@b1-systems.de>
2019-10-06 12:30:55 +0200

    Add example facter config file and example manifests.

---
+++ manifests/puppet_dsl_part2/facter2.pp
@@ -0,0 +1,5 @@
+# copyright: B1 Systems GmbH <info@b1-systems.de>, 2019
+# license:   GPLv3+, http://www.gnu.org/licenses/gpl-3.0.html
+
+notice( "Ruby Version: ${facts['ruby']['version']}" )
+notice( 'Ruby Version:', fact('ruby.version') )
---
+++ manifests/puppet_dsl_part2/facter_trusted.pp
@@ -0,0 +1,14 @@
+# copyright: B1 Systems GmbH <info@b1-systems.de>, 2019
+# license:   GPLv3+, http://www.gnu.org/licenses/gpl-3.0.html
+
+notice( $clientcert )
+notice( $trusted['certname'] )
+
+notice( $environment )
+notice( $server_facts['environment'] )
+
+notice( $trusted )
+notice( $server_facts )
+
+notice( $settings::all_local )
+notice( $settings::confdir )
