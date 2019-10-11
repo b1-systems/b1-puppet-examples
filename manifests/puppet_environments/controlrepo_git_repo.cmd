c2421b4472840cb78f8914a6eb2d4b0160b17372
Peter Hoffmann <hoffmann@b1-systems.de>
2019-10-11 18:07:16 +0200

    Work on chapter puppet_environments.

--- manifests/puppet_environments/controlrepo_git_repo.cmd
+++ manifests/puppet_environments/controlrepo_git_repo.cmd
@@ -1,6 +1,8 @@
 # copyright: B1 Systems GmbH <info@b1-systems.de>, 2018
 # license:   GPLv3+, http://www.gnu.org/licenses/gpl-3.0.html
 
+gitserver # yum install -y git
+
 gitserver # mkdir /srv/git
 gitserver # chown git. /srv/git
 gitserver # su - git
--- manifests/puppet_environments/r10k_install_puppet4.cmd
+++ manifests/puppet_environments/r10k_install_puppet4.cmd
@@ -2,5 +2,4 @@
 # license:   GPLv3+, http://www.gnu.org/licenses/gpl-3.0.html
 
 # /opt/puppetlabs/puppet/bin/gem install r10k
-# ln -sv /opt/puppetlabs/puppet/bin/r10k \
-  /usr/local/bin
+# ln -sv /opt/puppetlabs/puppet/bin/wrapper.sh /opt/puppetlabs/bin/r10k
