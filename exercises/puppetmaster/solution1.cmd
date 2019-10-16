03d4371fbd04bb6e97041f4fededef7972e41c7d
Peter Hoffmann <hoffmann@b1-systems.de>
2019-10-16 22:46:01 +0200

    Work on exercises puppetmaster.

--- exercises/puppetmaster/solution1.cmd
+++ exercises/puppetmaster/solution1.cmd
@@ -1,7 +1,7 @@
 # copyright: B1 Systems GmbH <info@b1-systems.de>, 2018
 # license:   GPLv3+, http://www.gnu.org/licenses/gpl-3.0.html
 
-puppet[screen 1]# cp solution1_site.pp /etc/puppetlabs/code/manifests/site.pp
+puppet[screen 1]# cp solution1_site.pp /etc/puppetlabs/code/environments/production/manifests/site.pp
 puppet[screen 1]# [puppet resource package puppet-server ensure=present]
 puppet[screen 1]# puppet resource package puppetserver ensure=present
 puppet[screen 1]# [puppet resource service puppetmaster ensure=stopped enable=false]
